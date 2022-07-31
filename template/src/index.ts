ac9cba9bf748dc51cc55660ce306268350f98a81
ikuma-t <tadokorodev@gmail.com>
2022-07-31 15:44:42 +0900

    feat: add ts-ignore for gas

--- template/src/index.ts
+++ template/src/index.ts
@@ -2,5 +2,7 @@ const main = (): void => {
   console.log('this is my template');
 }
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 declare let global: any
 global.main = main
